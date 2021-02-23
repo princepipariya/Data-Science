c8ebb562bc655edbdbcd50e57761604d597a0148
Prince Pipariya <63916960+princepipariya@users.noreply.github.com>
2021-02-23 12:14:25 +0530

    Random Word Generatoe
    Updated

--- Random_Word_Generator_1.py
+++ Random_Word_Generator_1.py
@@ -15,16 +15,18 @@ while True :
 	random_word = random.choice(word_list)
 	if repet == 'y' or repet == 'Y' or repet == '' :	
 		print(random_word)
-	answer = input('Do you wanna see the answer?')
-	if answer == 'y' or answer == 'Y' or answer == '' :
-		i = 0
-		for x in word_list:
-			i = i + 1
-			if x == random_word:
-				print(i)
-				break;
-		print('\n') 
-		
+		answer = input('Do you wanna see the answer?')
+		if answer == 'y' or answer == 'Y' or answer == '' :
+			i = 0
+			for x in word_list:
+				i = i + 1
+				if x == random_word:
+					print(i)
+					break;
+			print('\n') 
+		else :
+			print('Good!')
+			print('\n')
 	else :
 		print('Thank You!')
 		print('\n')
