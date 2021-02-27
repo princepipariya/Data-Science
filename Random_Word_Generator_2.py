0698a11428c44b52422eae4ce97af4d67c069b0d
Prince Pipariya <63916960+princepipariya@users.noreply.github.com>
2021-02-27 19:19:00 +0530

    Update Random_Word_Generator_2.py

--- Random_Word_Generator_2.py
+++ Random_Word_Generator_2.py
@@ -1,23 +1,33 @@
 import random 
  
-word_list = [	'1.) Filibuster', '2.) Inured', '3.) Tenuous', '4.) Precipitate', '5.) Martial', 
-				'6.) Machinist', '7.) Pristine', '8.) Feckless', '9.) Tenacity', '10.) Perfunctory', 
-				'11.) Ossified', '12.) Impersonate', '13.) Superfluous', '14.) Parody', '15.) Belie', 
-				'16.) Rescind', '17.) Obstensible', '18.) Sibilint', '19.) Qualm', '20.) Perfidy', 
-				'21.) Repudiate', '22.) Consternated', '23.) Exculpate', '24.) Arduous', '25.) Stupefy', 
-				'26.) Blase', '27.) Alacrity', '28.) Approbation', '29.) Axiom', '30.) Canon', 
-				'31.) Capricious', '32.) Convoluted', '33.) Disabuse', '34.) Effrontery', '35.) Exigent', 
-				'36.) Fluminate', '37.) Irascible', '38.) Prattle', '39.) Prevaricate', '40.) Predilection', 
-				'41.) Recant', '42.) Relagate', '43.) Sordid', '44.) Squander', '45.) Stymie',
-				'46.) Tortuous', '47.) Trruculent', '48.) Veracity', '49.) Virulent', '50.) Paean', 
+word_list = [	'Facetious', 'Doctrinaire', 'Didactic', 'Opprobrium', 'Avarice', 
+				'Heretical', 'Empirical', 'Reconvene', 'Rebound', 'Abrogate', 
+				'Staid', 'Eclectic', 'Bedraggled', 'Morose', 'Chary', 
+				'Contrite', 'Detestable', 'Feign', 'Ingratiate', 'Complaisance', 
+				'Retreat', 'Plodding', 'Potpourri', 'Exiguous', 'Surfeit', 
+				'Fortuious', 'Vista', 'Bloster', 'Pedantic', 'Occlude', 
+				'Lambast', 'Inchoate', 'Exscind', 'Descant', 'Refulgent', 
+				'Sojourn', 'Infelicitous', 'Precursor', 'Guileless', 'Liturgy'
 			] 
-
+print('HF - 2\n')
 while True :
 	repet = input('Press Y, y or ' ' for New Word: ')
+	random_word = random.choice(word_list)
 	if repet == 'y' or repet == 'Y' or repet == '' :	
-		random_word = random.choice(word_list) 
-		print(random_word)
-		print('\n')
+		print("=> " + random_word)
+		answer = input('Do you wanna see the answer?')
+		if answer == 'y' or answer == 'Y' or answer == '' :
+			i = 0
+			for x in word_list:
+				i = i + 1
+				if x == random_word:
+					temp = str(i) + ". => " + x 
+					print(temp)
+					break;
+			print('\n') 
+		else :
+			print('Good!')
+			print('\n')
 	else :
 		print('Thank You!')
 		print('\n')
